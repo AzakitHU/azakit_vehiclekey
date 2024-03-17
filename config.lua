072cd07779c9da09e26680ecd9434f1e52ee4c51
Azakit <155274165+AzakitHU@users.noreply.github.com>
2024-03-17 14:50:30 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@ Config = {}
 
 KeyPrice = 150 -- The price of creating a key
 LockKey = 303 -- [U] - opening/closing of vehicle
+EngineKey = 47 -- [G] - engine start/stop
 
 ITEM = "vehicle_key"  -- The basic vehicle key item. Meta data will always be added to this.
 OWNER = false  -- If this is true, then the owner of the vehicle can open/lock his car without a vehicle_key item
